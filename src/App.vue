eb1caa381c07d145b0bf94812a6d494de5973e12
gracebir <grace.devolop@gmail.com>
2024-05-24 12:33:50 +0200

    test the api

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,31 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <h1 class="text-xl font-semibold">hello world</h1>
+    <pre>{{ runs }}</pre>
+  </main>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script setup lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
 
-export default defineComponent({
-  components: {
-    HelloWorld,
-    RouterLink,
-    RouterView
-  }
+type runType = { id: number, title: string, startOn: string, completeOn: string, location: string, version: number }
+
+const runs = ref<runType[]>([])
+
+async function getRuns() {
+  const response = await fetch('http://localhost:8080/api/runs')
+  const data: runType[] = await response.json()
+  runs.value = data && data || []
+}
+
+onMounted(() => {
+  getRuns()
 })
+
+
 </script>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
